0db0be0f7efbc884f655b2a0eabe25a071757080
Sxoony <zamirkruger92@gmail.com>
2026-08-03 10:57:13 +0200

    Add project files.

---
+++ ITEHA3-B33-Project-Tyger Valley Campus-EDUV4848998/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using ITEHA3_B33_Project_Tyger_Valley_Campus_EDUV4848998.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITEHA3_B33_Project_Tyger_Valley_Campus_EDUV4848998.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ITEHA3-B33-Project-Tyger Valley Campus-EDUV4848998/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITEHA3_B33_Project_Tyger_Valley_Campus_EDUV4848998.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
